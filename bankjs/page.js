20a6b3e0ba43bc0a050fdaf57a8860a13f7f7439
shakil-py <shakilhaquepy@gmail.com>
2023-05-12 06:59:56 +0600

    add botstarap also

--- bankjs/page.js
+++ bankjs/page.js
@@ -1,9 +1,30 @@
-const email=document.getElementById("Email").value;
-const password=document.getElementById("password").value;
+document.getElementById("Email").addEventListener("keyup", function () {
+    const email = document.getElementById("Email").value
+    // ancor()
+    if (email.includes("@") && pas()) {
+        document.getElementById("submit-btn").removeAttribute("disabled")
+    }
 
-const pass="shakil"
-if(email.includes("@")){
-    document.getElementById("submit-btn").removeAttribute("disabled");
-    console.log("ok")
+});
+document.getElementById("password").addEventListener("keyup", function pas() {
+    let pass = document.getElementById("password").value
+    const password = "shakil123"
+    if (pass === password) {
+        document.getElementById("submit-btn").removeAttribute("disabled")
+    }
+})
 
-}
+document.getElementById("submit-btn2").addEventListener("mouseenter",function ancor(){
+    let ancor= document.getElementById("sumit-btn2").createElemnt("a")
+    ancor.href="bank.html"
+    document.getElementById("submit-btn2").appendChild(ancor)
+})
+
+// const password=document.getElementById("password").value;
+
+// const pass="shakil"
+// if(email.includes("@gmail")){
+//     document.getElementById("submit-btn").removeAttribute("disabled");
+//     console.log("ok")
+
+// }
