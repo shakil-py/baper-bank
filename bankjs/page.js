d8b4cc53b26e07d8ffe8c82728ea3cf8abceaa73
shakil-py <shakilhaquepy@gmail.com>
2023-05-12 08:16:18 +0600

    add link file

--- bankjs/page.js
+++ bankjs/page.js
@@ -1,24 +1,24 @@
-document.getElementById("Email").addEventListener("keyup", function () {
-    const email = document.getElementById("Email").value
-    // ancor()
-    if (email.includes("@") && pas()) {
-        document.getElementById("submit-btn").removeAttribute("disabled")
-    }
+// document.getElementById("Email").addEventListener("focuse", function () {
+//     const email = document.getElementById("Email").value
+//     // ancor()
+//     if (email.includes("@") && pas()) {
+//         document.getElementById("submit-btn").removeAttribute("disabled")
+//     }
 
-});
-document.getElementById("password").addEventListener("keyup", function pas() {
-    let pass = document.getElementById("password").value
-    const password = "shakil123"
-    if (pass === password) {
-        document.getElementById("submit-btn").removeAttribute("disabled")
-    }
-})
+// });
+// document.getElementById("password").addEventListener("keyup", function pas() {
+//     let pass = document.getElementById("password").value
+//     const password = "shakil123"
+//     if (pass === password) {
+//         document.getElementById("submit-btn").removeAttribute("disabled")
+//     }
+// })
 
-document.getElementById("submit-btn2").addEventListener("mouseenter",function ancor(){
-    let ancor= document.getElementById("sumit-btn2").createElemnt("a")
-    ancor.href="bank.html"
-    document.getElementById("submit-btn2").appendChild(ancor)
-})
+// document.getElementById("submit-btn2").addEventListener("click",function ancor(){
+//     console.log("clicked")
+//     HTMLAnchorElement("bank.html")
+
+// })
 
 // const password=document.getElementById("password").value;
 
@@ -28,3 +28,18 @@ document.getElementById("submit-btn2").addEventListener("mouseenter",function an
 //     console.log("ok")
 
 // }
+
+document.getElementById("submit-btn").addEventListener("click", function () {
+    const email_field = document.getElementById("Email")
+    const email = email_field.value
+    const password_field = document.getElementById("password")
+    const password = password_field.value
+
+    if (email === "shakil@gmail.com" && password === "123") {
+        window.location.href = "bank.html"
+    }
+    else {
+        alert("You are not valid user")
+    }
+
+})
