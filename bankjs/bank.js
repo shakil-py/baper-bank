fd04a91a6992a7f9cb3b44cdc2d6ed7c3221b5f9
shakil-py <shakilhaquepy@gmail.com>
2023-05-20 16:52:41 +0600

    add

--- bankjs/bank.js
+++ bankjs/bank.js
@@ -1,17 +1,17 @@
 
 document.getElementById("withdraw").addEventListener("click", function () {
     const withdraw_amount_field = document.getElementById("withdraw-amount").value
-
     const balance = document.getElementById("balance-amount").innerText
-
     document.getElementById("withdraw-balance").innerText = parseFloat(withdraw_amount_field) + parseFloat(document.getElementById("withdraw-balance").innerText)
-    document.getElementById("balance-amount").innerText = (parseInt(balance) - withdraw_amount_field)
     withdraw_amount_field.value = " "
+    document.getElementById("balance-amount").innerText = (parseInt(balance) - withdraw_amount_field)
+    
 })
 document.getElementById("diposit").addEventListener("click", function () {
     const diposit_amount_field = document.getElementById("diposit-amount").value
     const balance = parseInt(document.getElementById("balance-amount").innerText) + parseInt(diposit_amount_field)
+    diposit_amount_field.value = " "
     document.getElementById("balance-amount").innerText = balance
     document.getElementById("diposit-balance").innerText = parseFloat(diposit_amount_field) + parseFloat(document.getElementById("diposit-balance").innerText)
-    diposit_amount_field.value = " "
+    
 })
