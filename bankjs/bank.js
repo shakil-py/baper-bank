2947199601cebc0375b0e39191a51f99846fb0b2
shakil-py <shakilhaquepy@gmail.com>
2023-05-21 10:17:33 +0600

    add

--- bankjs/bank.js
+++ bankjs/bank.js
@@ -3,7 +3,7 @@ document.getElementById("withdraw").addEventListener("click", function () {
     const withdraw_field = document.getElementById("withdraw-amount")
     let withdraw_amount = withdraw_field.value
     if (withdraw_field.value === "") {
-        withdraw_amount = "0"
+        withdraw_amount = 0
         alert("Please Enter amount to Withdraw")
     }
     withdraw_field.value = ""
@@ -17,7 +17,7 @@ document.getElementById("diposit").addEventListener("click", function () {
     const diposit_field = document.getElementById("diposit-amount")
     let diposit_amount = diposit_field.value
     if (diposit_field.value === "") {
-        diposit_amount = "0"
+        diposit_amount = 0
         alert("Please Enter amount to Diposid")
     }
     diposit_field.value = ""
