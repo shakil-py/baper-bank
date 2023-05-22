1c7697ade69da50b24fd3827ec2aa5755036dc21
shakil-py <shakilhaquepy@gmail.com>
2023-05-22 14:02:13 +0600

    add all funtion component

--- bap er fun/bapfun.js
+++ bap er fun/bapfun.js
@@ -12,20 +12,20 @@ function get_tex_value_byid(text_id) {
     return num_value
 }
 function set_text_value(text_value_id, input_value, balance_value) {
-    const set_text_vlaue = document.getElementById(text_value_id)
-    set_text_vlaue.innerText = input_value + balance_value
+    const set_text_value = document.getElementById(text_value_id)
+    set_text_value.innerText = input_value + balance_value
 }
 function set_balance_value(balance_text_id, input_value, balance_text_value, compare_id) {
     const set_balance_field = document.getElementById(balance_text_id)
     const compare = document.getElementById(compare_id).innerText
-    const value=balance_text_value+input_value
+
 
     if (compare === "Withdraw") {
         set_balance_field.innerText = balance_text_value - input_value
 
     }
     else if (compare === "Diposit") {
-        set_balance_field.innerText = value
+        set_balance_field.innerText = input_value + balance_text_value
     }
 }
 
@@ -37,13 +37,11 @@ document.getElementById("withdraw").addEventListener("click", function () {
     const balance_text_value = get_tex_value_byid("balance-amount")
     set_text_value("withdraw-balance", input_value, withdraw_balance_value)
     set_balance_value("balance-amount", input_value, balance_text_value, "withdraw")
-
-
 })
 document.getElementById("diposit").addEventListener("click", function () {
-    const input_value = get_tex_value_byid("diposit-amount")
+    const input_value = get_value_byid("diposit-amount")
     const diposit_balance_value = get_tex_value_byid("diposit-balance")
     const balance_text_value = get_tex_value_byid("balance-amount")
     set_text_value("diposit-balance", input_value, diposit_balance_value)
-    set_balance_value("blance-amount", input_value, balance_text_value, "diposit")
+    set_balance_value("balance-amount", input_value, balance_text_value, "diposit")
 })
